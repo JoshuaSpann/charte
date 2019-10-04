9af4e54a025465a7bf06fcc79910c2ebc838b000
Joshua Spann <jspann128@gmail.com>
2019-10-04 16:27:57 -0500

    [~] Plugins Cleanup
    ~ pluginsJS searches for the plugin name as folder
    ~ Default plugin entry point is in myPluginFolder/plugin.js
    
    [jspann]

--- src/plugins.js
+++ src/plugins.js
@@ -6,11 +6,11 @@ let pluginScripts = [
 for (let scripts_i in pluginScripts) {
 	let pluginName = `plugins/${pluginScripts[scripts_i]}`
 
-	let scriptName = `${pluginName}.js`
+	let scriptName = `${pluginName}/plugin.js`
 	let script = document.createElement('script')
 	script.src = scriptName
 
-	let styleName = `${pluginName}.css`
+	let styleName = `${pluginName}/plugin.css`
 	let style = document.createElement('link')
 	style.href = styleName
 	style.rel = 'stylesheet'
